e082dc04917d1fd67fc3ccd4f6c30c140bd19da1
pruiz5514 <pablo_ruiza@hotmail.com>
2024-05-30 10:58:26 -0500

    Se inicializa proyecto

---
+++ app/header/header.js
@@ -0,0 +1,10 @@
+export const header = () => {
+    const header = document.createElement("section");
+
+    header.innerHTML = 
+    `
+        <article>
+            <h1></h1>
+        </article>
+    `
+}
